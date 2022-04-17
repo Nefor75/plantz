cadfe12a90b0c9bd7ec10dd2fc305737aa271726
Nefor75 <52212013+Nefor75@users.noreply.github.com>
2022-04-17 13:46:05 +0300

    Delete server_part directory

--- server_part/js/current_time.js
+++
@@ -1,8 +0,0 @@
-var myVar = setInterval(function() {
-  myTimer();
-}, 1000);
-
-function myTimer() {
-  var d = new Date();
-  document.getElementById("time-current").innerHTML = d.toLocaleTimeString()+" (Текущее время)";
-}
